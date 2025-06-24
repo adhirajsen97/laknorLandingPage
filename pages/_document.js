047838656784ffb4dd6da205763bcccf2cf02a10
adhirajsen97 <adhiraj.msm@gmail.com>
2025-06-24 20:55:20 +0530

    favicon browser icon

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Favicon - TODO: Replace with actual favicon */}
+        {/* Modern SVG Favicon */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        
+        {/* Fallback Favicons */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        
+        {/* Theme color for mobile browsers */}
+        <meta name="theme-color" content="#14b8a6" />
+        <meta name="msapplication-TileColor" content="#14b8a6" />
         
         {/* Preconnect to external domains for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
